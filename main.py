2b69d91639c7e1cec51ca9faeefd742d340b9031
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-02-12 09:12:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,50 @@
-with open('file.txt', "r") as word_list:
-	words = word_list.read().split(' ')
+# library for definitions
+from nltk.corpus import wordnet as wn
 
+
+
+end = False
+
+# Load input text
+with open('file.txt', 'r') as word_list:
+    file = word_list.read().split(' ')
+
+words = open('file.txt', 'r')
+
+# len parameter
+length = int(input("length : "))
+
+
+# Filtered list of vocab words
 filtered = []
 
 
-for i in words:
-	if len(i) >= 15 and i.isalpha() == True:
-		filtered.append(str(i))
-		
-print(filtered)
-print(len(filtered))
+# filtering process
 
-filter = open('output.txt', 'w')
-filter.write(str(filtered))
-filter.close()
-#any(i.isdigit() for i in filtered)
+# by length
+
+for i in file:
+    if len(i) >= length and i.isalpha() == True:
+        filtered.append(str(i))
+
+
+unique = list(set(filtered))
+
+# list of unique words
+
+print(unique)
+print(len(unique))
+
+
+# write to output.txt
+with open("output.txt", 'w') as f:
+    f.truncate(0)
+    f.write(str(unique))
+
+
+# definitions
+while end is not True:
+    thing = str(input("word: "))
+    syns = wn.synsets(thing)
+    print(syns[0].definition())
+
